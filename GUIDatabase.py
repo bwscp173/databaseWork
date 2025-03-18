b587f95d08badf910c8b1730e75793a442854a70
bwscp173 <benedictwardscp173@gmail.com>
2025-03-18 10:14:24 +0000

    added the tools page + offline mode
    added the tools page for running ANY sql command directly to the db
    i see no issues with this at all......
    self.__dbconnection can be None if the connection isnt established
    allowing the GUI to run without a connection to the db

--- GUIDatabase.py
+++ GUIDatabase.py
@@ -31,24 +31,20 @@ try:
                             database = secretData["database"]
                             )
 except psycopg2.OperationalError as e:
-    print("connection error, turn on the 'BIG-IP edge client' vpn ")
-    exit(-1)
-# given example of the psycopg2 api:
-# cur = conn.cursor()
-# cur.execute('SET search_path TO Demo,public;')
-# cur.execute('SELECT * FROM emp')
-# rows = cur.fetchall()
-#
-# for row in rows:
-#     print(row[0], row[1], row[3])
-# conn.close()
+    print("connection error, turn on the 'BIG-IP edge client' vpn.")
+    print("running GUI but commands won't execute.")
+    conn = None
+
+
 
 class App(CTk.CTk):
-    def __init__(self, database_connection:psycopg2.extensions.connection):
+    def __init__(self, database_connection:psycopg2.extensions.connection) -> None:
+        """uses the connection to generate a database cursor and initialises the GUI"""
         super().__init__()
         self.__DbConnection = database_connection
-        self.cursor = self.__DbConnection.cursor()
-        self.cursor.execute('SET search_path to summative,public;')
+        if self.__DbConnection is not None:
+            self.cursor = self.__DbConnection.cursor()
+            self.cursor.execute('SET search_path to summative,public;')
 
         self.geometry("550x450")
         self.__title = "SQL summative project"
@@ -69,33 +65,30 @@ class App(CTk.CTk):
         self.frames.append(self.__tabController.add("Inserting"))
         self.frames.append(self.__tabController.add("Update"))
         self.frames.append(self.__tabController.add("Delete"))
+        self.frames.append(self.__tabController.add("Tools"))
+        self.frames.append(self.__tabController.add("Results"))
 
         # the default GUI on each of the tabs
         self.initiliseFrame1()
         self.initiliseFrame2()
         self.initiliseFrame3()
         self.initiliseFrame4()
+        self.initiliseFrame5()
+        self.initiliseFrame6()
 
         # Specify width and height in the constructor
         self.frames[0].bind('<Enter>', lambda _: self.title(self.__title+" - Selecting Data"))
         self.frames[1].bind('<Enter>', lambda _: self.title(self.__title+" - Inserting Data"))
         self.frames[2].bind('<Enter>', lambda _: self.title(self.__title+" - Updating Data"))
         self.frames[3].bind('<Enter>', lambda _: self.title(self.__title+" - Deleting Data"))
+        self.frames[4].bind('<Enter>', lambda _: self.title(self.__title+" - Running any Command"))
+        self.frames[5].bind('<Enter>', lambda _: self.title(self.__title + " - Looking at Data"))
 
         self.frame1totalbuttons = 0
+        self.mostRecentQuery = ""
 
-    def initiliseFrame1(self):
+    def initiliseFrame1(self) -> None:
         """The Gui for the Selecting Tab"""
-        tableName = self.__tableNameTitles.keys()
-        tableTitles = self.__tableNameTitles["exam"]
-        # button = CTk.CTkButton(self.frames[0], text="fetch exam", command=lambda:self.displayDataFromTable("exam"))
-        # button.grid(column=2, row= 0,padx=20, pady=20)
-        # button = CTk.CTkButton(self.frames[0], text="fetch student", command=lambda:self.displayDataFromTable("student"))
-        # button.grid(column=2, row= 1,padx=20, pady=20)
-        # button = CTk.CTkButton(self.frames[0], text="fetch entry", command=lambda:self.displayDataFromTable("entry"))
-        # button.grid(column=2, row= 2,padx=20, pady=20)
-        # button = CTk.CTkButton(self.frames[0], text="fetch cancel", command=lambda:self.displayDataFromTable("cancel"))
-        # button.grid(column=2, row= 3,padx=20, pady=20)
 
         label = CTk.CTkLabel(self.frames[0], text="Chose from a table")
         label.grid(column=0, row=0)
@@ -174,14 +167,6 @@ class App(CTk.CTk):
         tableName = dropDownOptionValue.get()
         self.frame1totalbuttons = len(self.__tableNameTitles[tableName])
 
-        # # deleting all the checkboxes
-        # i=0
-        # while True:
-        #     try:
-        #         allButtons[i].destroy()
-        #         i += 1
-        #     except IndexError:
-        #         break
         for i in range(len(allButtons)):
             allButtonsText[i].set("_________")
             allButtons[i].lower()
@@ -189,7 +174,7 @@ class App(CTk.CTk):
 
         totalQueryColumns = []
 
-        for i in range(self.frame1totalbuttons):
+        for i in range(self.frame1totalbuttons-1):
             allButtons[i].forget()  # I have forgotten what this even does ong
             allButtons[i].configure(state=CTk.NORMAL)
             print(i,self.__tableNameTitles[tableName][i])
@@ -203,10 +188,20 @@ class App(CTk.CTk):
             totalQueryColumns = ["*"]
 
         if WHERETextVar.get() == "":
-            totalQueryVar.set(f"total query:\nSELECT {(", ".join(totalQueryColumns))}\n FROM {tableName};")
+            self.mostRecentQuery = f"SELECT {(", ".join(totalQueryColumns))}\n FROM {tableName};"
+            totalQueryVar.set(f"total query: {self.mostRecentQuery}")
 
         else:
-            totalQueryVar.set(f"total query:\nSELECT {(", ".join(totalQueryColumns))}\n FROM {tableName} WHERE \n{WHERETextVar.get()};")
+            self.mostRecentQuery = f"SELECT {(", ".join(totalQueryColumns))}\n FROM {tableName} WHERE \n{WHERETextVar.get()};"
+            totalQueryVar.set(f"total query:\n{self.mostRecentQuery}")
+
+        print("executing: "+self.mostRecentQuery)
+        if self.__DbConnection != None:
+            self.cursor.execute(self.mostRecentQuery)
+            print(self.cursor.fetchall())
+        #self.__DbConnection.commit()
+
+
 
 
     def initiliseFrame2(self):
@@ -225,6 +220,7 @@ class App(CTk.CTk):
 
         dropDownOptions = list(self.__tableNameTitles.keys())
         dropDownTableOptions = CTk.CTkOptionMenu(self.frames[1],variable=dropDownOptionValue,values=dropDownOptions)
+        dropDownTableOptions.set(dropDownOptions[0])
         dropDownTableOptions.grid(column=1,row=0, padx=20, pady=20)
 
         label = CTk.CTkLabel(self.frames[1], text="type in the parameters\nin a CSV format")
@@ -253,13 +249,15 @@ class App(CTk.CTk):
     def __insertIntoDb(self, dropDownOptionValue:CTk.StringVar, entryTextVar:CTk.StringVar):
         print("INSERT INTO " + dropDownOptionValue.get() + "(" + (", ".join(
         self.__tableNameTitles[dropDownOptionValue.get()])) + ")" + "VALUES\n(" + entryTextVar.get() + ")"),
-        self.cursor.execute(f"INSERT INTO {dropDownOptionValue.get()} VALUES\n({entryTextVar.get()})"),
-        self.__DbConnection.commit()
+        if self.__DbConnection is not None:
+            self.cursor.execute(f"INSERT INTO {dropDownOptionValue.get()} VALUES\n({entryTextVar.get()})"),
+            self.__DbConnection.commit()
 
     def __updateDb(self):
         pass
 
     def initiliseFrame3(self):
+        """the Frame for the update GUI"""
         entryTextVar = CTk.StringVar()
         requirements = CTk.StringVar()
         totalQueryVar = CTk.StringVar()
@@ -272,28 +270,76 @@ class App(CTk.CTk):
 
         dropDownOptions = list(self.__tableNameTitles.keys())
         dropDownTableOptions = CTk.CTkOptionMenu(self.frames[2], variable=dropDownOptionValue, values=dropDownOptions)
+        dropDownTableOptions.set(dropDownOptions[0])
         dropDownTableOptions.grid(column=1, row=0, padx=20, pady=20)
+
+        label = CTk.CTkLabel(self.frames[2], text="Set the new values:")
+        label.grid(column=0, row=1, padx=20, pady=20)
+        label = CTk.CTkLabel(self.frames[2], text="WHERE:")
+        label.grid(column=0, row=2, padx=20, pady=20)
+
+        entryTextVar1 = CTk.StringVar()
+        entryTextVar2 = CTk.StringVar()
+
+        textEntry1 = CTk.CTkEntry(self.frames[2], textvariable=entryTextVar1)
+        textEntry2 = CTk.CTkEntry(self.frames[2], textvariable=entryTextVar2)
+        textEntry1.grid(column=1, row=1, padx=20, pady=20)
+        textEntry2.grid(column=1, row=2, padx=20, pady=20)
 
         updateButton = CTk.CTkButton(self.frames[2], text="Update 'total query'", command=
         lambda: (
             totalQueryVar.set(f"total query:\nUPDATE {dropDownTableOptions.get()} SET{(", ".join(
                 self.__tableNameTitles[dropDownOptionValue.get()]))} VALUES\n(" + entryTextVar.get() + ")"),
             requirements.set("Requirements\n" + (", ".join(self.__tableNameTitles[dropDownOptionValue.get()])))))
-        updateButton.grid(column=0, row=2, padx=20, pady=20)
+        updateButton.grid(column=0, row=3, padx=20, pady=20)
 
         label = CTk.CTkLabel(self.frames[2], textvariable=totalQueryVar)
-        label.grid(column=1, row=2, padx=20, pady=20)
+        label.grid(column=1, row=3, padx=20, pady=20)
 
     def initiliseFrame4(self):
+        """the Frame for the Delete GUI"""
+        pass
+
+    def initiliseFrame5(self):
+        """the Frame for the Tools GUI"""
+        commandInp = CTk.StringVar()
+        requirements = CTk.StringVar()
+        totalQueryVar = CTk.StringVar()
+        totalQueryVar.set("total query: ")
+
+        text_tables = f"Chose from a table: {', '.join(list(self.__tableNameTitles.keys()))}"
+
+        label = CTk.CTkLabel(self.frames[4], text=text_tables)
+        label.grid(column=0, row=0, padx=20, pady=20)
+
+        label = CTk.CTkLabel(self.frames[4], text="what command to run:")
+        label.grid(column=0, row=1, padx=20, pady=20)
+        textEntry1 = CTk.CTkEntry(self.frames[4], textvariable=commandInp)
+        textEntry1.grid(column=1,row=1)
+
+        updateButton = CTk.CTkButton(self.frames[4], text="execute command", command=
+        lambda: self.__runAnyCommand(commandInp.get()))
+        updateButton.grid(column=0, row=2, padx=20, pady=20)
+
+    def __runAnyCommand(self,command:str):
+        """runs ANY sql command onto the database without any checks
+        SQL injection who?"""
+        if self.__DbConnection is not None:
+            self.cursor.execute(command)
+            self.__DbConnection.commit()
+
+
+    def initiliseFrame6(self):
+        """the Frame for the Results GUI"""
         pass
 
     def displayDataFromTable(self, tableName:str )-> None:
         allColumns = self.__tableNameTitles[tableName]
+        if self.__DbConnection is not None:
+            self.cursor.execute(f'SELECT * FROM {tableName}')
+            rows = self.cursor.fetchall()
 
-        self.cursor.execute(f'SELECT * FROM {tableName}')
-        rows = self.cursor.fetchall()
-
-        print(rows)
+            print(rows)
 
 try:
     app = App(conn)
